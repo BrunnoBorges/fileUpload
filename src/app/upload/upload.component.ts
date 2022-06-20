9cf397aa9a85d93f2e9d6d6ecd84e161e51e7b04
bliteti.brunno <bliteti.brunno@umidas.com.br>
2022-06-20 18:41:09 -0300

    criando upload de arquivo, front angular - back node

---
+++ src/app/upload/upload.component.ts
@@ -0,0 +1,45 @@
+import { UploadService } from './upload.service';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-upload',
+  templateUrl: './upload.component.html',
+  styleUrls: ['./upload.component.scss']
+})
+export class UploadComponent implements OnInit {
+
+  @ViewChild('fileInput', {static: false}) fileInput?: ElementRef;
+
+  files?: Set<File>;
+  fileType: any;
+  fileToUpload = new FormData();
+  selectedFiles?: FileList;
+
+  constructor(private uploadService: UploadService) { }
+
+  ngOnInit(): void {
+  }
+
+  onChange(event: any) {
+    
+    this.selectedFiles = <FileList>event.srcElement.files;
+
+    this.fileType = this.selectedFiles[0].name;
+    const fileNames = [];
+    this.files = new Set();
+    for(let i = 0; i < this.selectedFiles.length; i++) {
+      fileNames.push(this.selectedFiles[i].name)
+      this.files.add(this.selectedFiles[i]);
+    }
+  }
+
+  onUpload() {
+
+    if(this.files && this.files.size > 0) {
+      console.log('sdsdsd', this.files);
+      this.uploadService.upload(this.files, 'http://localhost:8000/upload' )
+        .subscribe(resp => console.log('Upload concluido'))
+    }
+  }
+
+}
